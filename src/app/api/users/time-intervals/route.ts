f22a54ab00759f67e7972478dee6bb5aa34856af
RicardoBrito1938 <ricardo.jucrist@hotmail.com>
2024-02-09 09:27:26 +0000

    feature: add interval

--- src/app/api/users/time-intervals/route.ts
+++ src/app/api/users/time-intervals/route.ts
@@ -1,18 +1,45 @@
-import { getServerSession } from "next-auth/next";
-import { config } from "../../auth/[...nextauth]/route";
-import { NextApiRequest, NextApiResponse } from "next";
+import { z } from "zod";
+import { auth } from "../../auth/[...nextauth]/route";
+import { prisma } from "@/lib/prisma";
 
-export async function POST(req: NextApiRequest, res: NextApiResponse) {
-  const session = await getServerSession(req, res, config);
+const timeIntervalsBodySchema = z.object({
+  intervals: z.array(
+    z.object({
+      weekDay: z.number().min(0).max(6),
+      startTimeInMinutes: z.number().min(0).max(1440),
+      endTimeInMinutes: z.number().min(0).max(1440),
+    })
+  ),
+});
 
-  console.log(res);
+export async function POST(request: Request) {
+  const session = await auth();
+  const requestBody = await request.json();
 
   if (!session) {
-    res.status(401).json({ message: "You must be logged in." });
-    return;
+    return Response.json({ error: "Unauthorized" }, { status: 401 });
   }
 
-  return res.json({
-    message: "Success",
-  });
+  const { intervals } = timeIntervalsBodySchema.parse(requestBody);
+
+  console.log(intervals);
+
+  // //time to create a proper SQL server, but not today Satan
+  // //for me in the future, instead of using Promise.all, we could use createMany, but sqlite does not support many inserts at once
+
+  await Promise.all(
+    intervals.map((interval) => {
+      return prisma.userTimeInterval.create({
+        data: {
+          week_day: interval.weekDay,
+          time_start_in_minutes: interval.startTimeInMinutes,
+          time_end_in_minutes: interval.endTimeInMinutes,
+          user_id: session.user?.id,
+        },
+      });
+    })
+  );
+
+  // //CQRS = Command Query Responsibility Segregation
+  return Response.json({ success: true }, { status: 201 });
 }
